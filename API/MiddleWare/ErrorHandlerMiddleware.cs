bf580f2530c180cd8a3cf66c02f0b008353a5357
habiba alaa <habibaalaa775@gmal.com>
2025-08-11 11:41:17 +0300

    imolemeted a custome middle ware for error handling

---
+++ API/MiddleWare/ErrorHandlerMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Text.Json;
+
+
+
+
+namespace API.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                var responseModel = new Response<string>()
+                {
+                    Succeeded = false,
+                    Message = error?.Message
+                };
+
+                switch (error)
+                {
+                    case UnauthorizedAccessException:
+                        responseModel.StatusCode = HttpStatusCode.Unauthorized;
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        break;
+
+                    case ValidationException:
+                        responseModel.StatusCode = HttpStatusCode.UnprocessableEntity;
+                        response.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                        break;
+
+                    case KeyNotFoundException:
+                        responseModel.StatusCode = HttpStatusCode.NotFound;
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    case DbUpdateException:
+                        responseModel.StatusCode = HttpStatusCode.BadRequest;
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+
+                    default:
+                        responseModel.StatusCode = HttpStatusCode.InternalServerError;
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                if (error.InnerException != null)
+                {
+                    responseModel.Message += "\n" + error.InnerException.Message;
+                }
+
+                var result = JsonSerializer.Serialize(responseModel);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+
+
+    public class Response<T>
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public T Data { get; set; }
+    }
+}
